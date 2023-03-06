fc30123d34fb6f235011848af766592e2bdcae52
ArD2ev <122788206+ArD2ev@users.noreply.github.com>
2023-03-06 21:58:36 +0300

    Update brzbot.py

--- brzbot.py
+++ brzbot.py
@@ -3,7 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Chrome()
-browser.get('http://ep4sh.cc/paste/add')
+browser.get('http://ep4sh.cc/')
+
+noteadded = '/html/body/div/div/div/div[2]/div/div/div/div/a/button'
+browser.find_element(By.XPATH, noteadded).click()
+
+added = '/html/body/section/div[2]/a'
+browser.find_element(By.XPATH, added).click()
 
 addedtext = '/html/body/div/form/div[1]/input'
 browser.find_element(By.XPATH, addedtext).send_keys('BRZ_BOT')
